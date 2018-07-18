a21a5e930eebff2b5f7b8127ab4d6e296217c837
lhr <2388399752@qq.com>
2018-07-18 09:29:19 +0800

    修改去广告规则
    修改去广告规则

--- v2ex/v2ex-helper.js
+++ v2ex/v2ex-helper.js
@@ -2,7 +2,7 @@
 // @name         V2ex-helper
 // @description  1.去除v2ex广告 2.添加百度和bing搜索
 // @copyright    2018, lhr(https://www.lhrsite.com/)
-// @version      1.1.0
+// @version      1.2.0
 // @author       lhr
 // @license      MIT
 // @homepageURL  https://github.com/zzlhr/tampermonkey-script/blob/master/v2ex/
@@ -30,11 +30,11 @@
         $(googles[i]).parent().parent().attr('hidden','')
     }
 
-    $('#Search').children('form').html($('#Search').children('form').html() + 
+    $('#Search').children('form').html($('#Search').children('form').html() +
     '<a id="baidu" style="padding-right:5px;" href="https://www.baidu.com/s?wd=site: www.lhrsite.com">百度</a><a id="google" href="https://www.google.com/search?q=site: www.lhrsite.com" style="padding-right:5px;">google</a><a href="https://cn.bing.com/search?q=site: www.lhrsite.com" style="padding-right:5px;" id="bing">bing</a>')
     //修改查询
     var base=['https://www.baidu.com/s?wd=site: v2ex.com ', 'https://www.google.com/search?q=site: v2ex.com/t ', 'https://cn.bing.com/search?q=site: v2ex.com/t ']
-    
+
 
     $('#qbar').children('input').change(function(){
         var value = $('#qbar').children('input').val()
@@ -42,5 +42,21 @@
         $('#google').attr('href', base[1]+value);
         $('#bing').attr('href', base[2]+value);
     });
+
+
+
+    //======2018-07-18 09:17:49=====
+    //==========修改广告规则==========
+    var inners = $('.inner');
+    console.log(inners)
+    for (i=0;i<inners.length;i++){
+        var item = inners[i]
+        item = $(item)
+        if(item.attr('align') === 'center'){
+             console.log(item)
+             item.hide()
+        }
+
+    }
     // Your code here...
 })();
